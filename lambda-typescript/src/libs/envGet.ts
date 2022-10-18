2e0d5e97473347763a908409f4ad64e55cc717d9
Sukairo-02 <reka.serzh@yandex.ru>
2022-10-18 18:10:11 +0300

    Added safe process.env data parser

---
+++ lambda-typescript/src/libs/envGet.ts
@@ -0,0 +1,21 @@
+type ObjectFromList<T extends ReadonlyArray<string>, V = string> = {
+	[K in T extends ReadonlyArray<infer U> ? U : never]: V
+}
+/**
+ * Safely get data from process.env
+ *
+ * Warning: return's type allows to access any field, however only the ones, names of which were passed, should be used
+ */
+const envGet = (...vars: ReadonlyArray<string>) => {
+	const varsParsed: ObjectFromList<typeof vars> = {}
+	for (const e in vars) {
+		if (!process.env[e]) {
+			throw new Error(`Error: ${e} is missing in process.env`)
+		}
+
+		varsParsed[e] = process.env[e]!
+	}
+	return varsParsed
+}
+
+export = envGet
