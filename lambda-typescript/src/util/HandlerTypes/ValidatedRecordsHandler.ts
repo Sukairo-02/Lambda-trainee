0e3b5050a608a81bf8765ac22eff52b0ce9a7e06
Sukairo-02 <reka.serzh@yandex.ru>
2022-10-30 23:21:51 +0200

    More accurate typing

--- lambda-typescript/src/util/HandlerTypes/ValidatedRecordsHandler.ts
+++ lambda-typescript/src/util/HandlerTypes/ValidatedRecordsHandler.ts
@@ -1,13 +1,11 @@
 import type { SQSBatchResponse, SQSRecord, Handler } from 'aws-lambda'
 
-type RecordsEvent<RecordType> = {
-	Records?: RecordType[]
-	FailRecords?: Partial<RecordType>[]
+type ValidatedRecordsEvent<RecordType, UpdateWith> = {
+	Records?: (Omit<RecordType, keyof UpdateWith> & UpdateWith)[]
+	FailRecords?: (Omit<RecordType, keyof UpdateWith> & Partial<UpdateWith>)[]
 }
 
-type ValidatedRecordsEvent<RecordType, UpdateWith> = RecordsEvent<Omit<RecordType, keyof UpdateWith> & UpdateWith>
-
-export type ValidatedRecordsHandler<UpdateWith, RecordType = SQSRecord, Returns = SQSBatchResponse> = Handler<
+export type ValidatedRecordsHandler<UpdateWith, RecordType = SQSRecord, Returns = SQSBatchResponse | void> = Handler<
 	ValidatedRecordsEvent<RecordType, UpdateWith>,
 	Returns
 >
