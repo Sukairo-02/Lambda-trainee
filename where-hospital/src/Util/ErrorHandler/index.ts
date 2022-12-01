ff5c76f0b781be224c9d5e6af97b5a5e6d9ca389
Sukairo-02 <reka.serzh@yandex.ru>
2022-12-02 01:27:46 +0200

    Fixed error handler not being identified as error handler

--- where-hospital/src/Util/ErrorHandler/index.ts
+++ where-hospital/src/Util/ErrorHandler/index.ts
@@ -1,11 +1,17 @@
 import { isBoom, Boom } from '@hapi/boom'
+import util from 'util'
 import type { ErrorRequestHandler } from 'express'
 
-export = <ErrorRequestHandler>((err, req, res) => {
+export = <ErrorRequestHandler>((err, req, res, next) => {
 	if (isBoom(err)) {
 		return res.status((<Boom>err).output.statusCode).json({ message: (<Boom>err).message })
 	}
 
-	console.error(`Error:\n${err}\nRequest:\n${req}`)
-	return res.status(500).json({ message: 'Unknown error occured... Try again later' })
+	console.log(typeof err, typeof req)
+	console.error(
+		`Error:\n${JSON.stringify(err)}\nPath:\n${req.path}\nBody:\n${util.inspect(req.body)}\nParams:\n${util.inspect(
+			req.params
+		)}\nQuery:\n${util.inspect(req.query)}\n`
+	)
+	return res.status(500).json({ error: 'Unknown error occured... Try again later' })
 })
