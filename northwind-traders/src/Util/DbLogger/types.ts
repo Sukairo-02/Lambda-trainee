d7f2658f8166914c764ebdad2d00418192964dcb
Sukairo-02 <reka.serzh@yandex.ru>
2022-12-07 19:12:06 +0200

    DbLogger update: now supports 'where'

--- northwind-traders/src/Util/DbLogger/types.ts
+++ northwind-traders/src/Util/DbLogger/types.ts
@@ -1,8 +1,19 @@
 import { AnyPgTable, SelectResultFields, PgSelectFields, GetTableConfig } from 'drizzle-orm-pg'
 import { PgDelete, PgInsert, PgSelect, PgUpdate } from 'drizzle-orm-pg/queries'
+import { JoinType } from 'drizzle-orm-pg/queries/select.types'
+
+type Insert = PgInsert<AnyPgTable, SelectResultFields<PgSelectFields<GetTableConfig<AnyPgTable, 'name'>>>>
+type Select = PgSelect<AnyPgTable, SelectResultFields<PgSelectFields<GetTableConfig<AnyPgTable, 'name'>>>>
+type Update = PgUpdate<AnyPgTable, SelectResultFields<PgSelectFields<GetTableConfig<AnyPgTable, 'name'>>>>
+type Delete = PgDelete<AnyPgTable, SelectResultFields<PgSelectFields<GetTableConfig<AnyPgTable, 'name'>>>>
+type AfterWhere<T extends Insert | Select | Update | Delete> = Omit<T, 'where' | `${JoinType}Join`>
 
 export type dbOperation =
-	| PgInsert<AnyPgTable, SelectResultFields<PgSelectFields<GetTableConfig<AnyPgTable, 'name'>>>>
-	| PgSelect<AnyPgTable, SelectResultFields<PgSelectFields<GetTableConfig<AnyPgTable, 'name'>>>>
-	| PgUpdate<AnyPgTable, SelectResultFields<PgSelectFields<GetTableConfig<AnyPgTable, 'name'>>>>
-	| PgDelete<AnyPgTable, SelectResultFields<PgSelectFields<GetTableConfig<AnyPgTable, 'name'>>>>
+	| Insert
+	| Select
+	| Update
+	| Delete
+	| AfterWhere<Insert>
+	| AfterWhere<Select>
+	| AfterWhere<Update>
+	| AfterWhere<Delete>
