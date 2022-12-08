cac24ff505b275940119fe7702189fec59cd2a7a
Sukairo-02 <reka.serzh@yandex.ru>
2022-12-08 16:53:55 +0200

    Updated DbLogger to support .fields()

--- northwind-traders/src/Util/DbLogger/types.ts
+++ northwind-traders/src/Util/DbLogger/types.ts
@@ -1,13 +1,14 @@
 import { AnyPgTable, SelectResultFields, PgSelectFields, GetTableConfig } from 'drizzle-orm-pg'
 import { PgDelete, PgInsert, PgSelect, PgUpdate } from 'drizzle-orm-pg/queries'
 import { JoinType } from 'drizzle-orm-pg/queries/select.types'
+import { SQLWrapper } from 'drizzle-orm/sql'
 
 type Insert = PgInsert<AnyPgTable, SelectResultFields<PgSelectFields<GetTableConfig<AnyPgTable, 'name'>>>>
 type Select = PgSelect<AnyPgTable, SelectResultFields<PgSelectFields<GetTableConfig<AnyPgTable, 'name'>>>>
 type Update = PgUpdate<AnyPgTable, SelectResultFields<PgSelectFields<GetTableConfig<AnyPgTable, 'name'>>>>
 type Delete = PgDelete<AnyPgTable, SelectResultFields<PgSelectFields<GetTableConfig<AnyPgTable, 'name'>>>>
 type AfterWhere<T extends Insert | Select | Update | Delete> = Omit<T, 'where' | `${JoinType}Join`>
-
+type AfterFields<T extends Insert | Select | Update | Delete> = Omit<T, 'fields'>
 export type dbOperation =
 	| Insert
 	| Select
@@ -17,3 +18,7 @@ export type dbOperation =
 	| AfterWhere<Select>
 	| AfterWhere<Update>
 	| AfterWhere<Delete>
+	| AfterFields<Insert>
+	| AfterFields<Select>
+	| AfterFields<Update>
+	| AfterFields<Delete>
