a610ea09624123e2a730164f661186ed63cf9971
Sukairo-02 <reka.serzh@yandex.ru>
2022-12-26 22:57:37 +0200

    Reworked due to ORM updates, improved typing

--- northwind-traders/src/Util/DbLogger/index.ts
+++ northwind-traders/src/Util/DbLogger/index.ts
@@ -1,11 +1,14 @@
-import type { DbOperation } from './types'
+import type { HasSQL } from './types'
 import Orm from '@Database/Northwind'
 
-export const dbLogger = async <T extends DbOperation>(dbQuery: T) => {
-	const db = await Orm.Connector.connect()
-	await db.insert(Orm.Tables.AdminLogs).values({
-		query: dbQuery.toSQL().sql
-	})
+export const dbLogger =
+	(tabUUID: string | undefined) =>
+	async <T extends HasSQL>(dbQuery: T) => {
+		const db = Orm.Connector
+		await db.insert(Orm.Tables.AdminLogs).values({
+			query: dbQuery.toSQL().sql,
+			sender: tabUUID
+		})
 
-	return dbQuery
-}
+		return dbQuery
+	}
--- northwind-traders/src/Util/DbLogger/types.ts
+++ northwind-traders/src/Util/DbLogger/types.ts
@@ -1,23 +1,6 @@
-import { AnyPgTable, SelectResultFields, PgSelectFields, GetTableConfig } from 'drizzle-orm-pg'
-import { PgDelete, PgInsert, PgSelect, PgUpdate } from 'drizzle-orm-pg/queries'
-import { JoinType } from 'drizzle-orm-pg/queries/select.types'
-
-type Insert = PgInsert<AnyPgTable, SelectResultFields<PgSelectFields<GetTableConfig<AnyPgTable, 'name'>>>>
-type Select = PgSelect<AnyPgTable, SelectResultFields<PgSelectFields<GetTableConfig<AnyPgTable, 'name'>>>>
-type Update = PgUpdate<AnyPgTable, SelectResultFields<PgSelectFields<GetTableConfig<AnyPgTable, 'name'>>>>
-type Delete = PgDelete<AnyPgTable, SelectResultFields<PgSelectFields<GetTableConfig<AnyPgTable, 'name'>>>>
-type AfterWhere<T extends Insert | Select | Update | Delete> = Omit<T, 'where' | `${JoinType}Join`>
-type AfterFields<T extends Insert | Select | Update | Delete> = Omit<T, 'fields'>
-export type DbOperation =
-	| Insert
-	| Select
-	| Update
-	| Delete
-	| AfterWhere<Insert>
-	| AfterWhere<Select>
-	| AfterWhere<Update>
-	| AfterWhere<Delete>
-	| AfterFields<Insert>
-	| AfterFields<Select>
-	| AfterFields<Update>
-	| AfterFields<Delete>
+export type HasSQL = {
+	toSQL: () => {
+		params: unknown[]
+		sql: string
+	}
+}
