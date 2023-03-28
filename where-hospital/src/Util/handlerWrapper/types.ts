5a4ca238dd42e231c0016bb1383c80774f2f10a4
Sukairo-02 <reka.serzh@yandex.ru>
2023-03-28 13:24:01 +0300

    Removed unused util

--- where-hospital/src/Util/handlerWrapper/types.ts
+++
@@ -1,7 +0,0 @@
-import type { TypedHandler } from '@Util/TypedHandler'
-import { z } from 'zod'
-
-const anySchema = z.any()
-type ZodAnySchema = typeof anySchema
-
-export type AnyTypedHandler = TypedHandler<ZodAnySchema, any>
